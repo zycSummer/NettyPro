7a7940f53e9d5635fe809afc0179f14cac8aa961
zycSummer <13584817949@163.com>
2021-01-11 11:28:41 +0800

    netty codec2

---
+++ src/main/java/com/zyc/netty/netty/codec2/NettyClientHandler.java
@@ -0,0 +1,39 @@
+package com.zyc.netty.netty.codec2;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+import java.util.Random;
+
+public class NettyClientHandler extends ChannelInboundHandlerAdapter {
+
+    //当通道就绪就会触发该方法
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        // 随机的发送student或者worker对象
+        int random = new Random().nextInt(3);
+        MyDataInfo.MyMessage myMessage = null;
+        if (random == 0) {
+            myMessage = MyDataInfo.MyMessage.newBuilder().setDataType(MyDataInfo.MyMessage.DataType.StudentType).setStudent(MyDataInfo.Student.newBuilder().setId(5).setName("JJ 林俊杰").build()).build();
+        } else {
+            myMessage = MyDataInfo.MyMessage.newBuilder().setDataType(MyDataInfo.MyMessage.DataType.WorkerType).setWorker(MyDataInfo.Worker.newBuilder().setAge(45).setName("JAY 周杰伦").build()).build();
+        }
+        ctx.writeAndFlush(myMessage);
+    }
+
+    //当通道有读取事件时，会触发
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        System.out.println("服务器回复的消息:" + buf.toString(CharsetUtil.UTF_8));
+        System.out.println("服务器的地址： " + ctx.channel().remoteAddress());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
